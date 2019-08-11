3a8cd644624f74f31cd811fb5db761c29a245bb3
Wynn Chaney <wynn.chaney@wanco.com>
2019-08-11 03:40:23 -0600

    Updating forms with id's for buttons for ease of access, creating update functionality.

--- routes/api/trips.js
+++ routes/api/trips.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const tripsController = require('../../controllers/tripsControllers');
-
+// var trips = require('../../models/trip.js');
 // Matches with "/api/trips"
 router.route('/')
   .get(tripsController.findAll)
@@ -9,7 +9,20 @@ router.route('/')
 // Matches with "/api/trips/:id"
 router
   .route('/:id')
-  .get(tripsController.findById)
+  // .get(tripsController.findById, function (req, res) {
+  //   trips.all(function (data) {
+  //     var tripsObject = { trips: data };
+  //     res.render('trips.pug', tripsObject);
+  //   });
+  // })
+  // router.get("/burgers", function(req, res) {
+  //   // express callback response by calling burger.selectAllBurger
+  //   burger.all(function(data) {
+  //     // Wrapping the array of returned burgers in a object so it can be referenced inside our handlebars
+  //     var hbsObject = { burgers: data };
+  //     res.render("index", hbsObject);
+  //   });
+  // });
   .put(tripsController.update)
   .delete(tripsController.remove);
 
